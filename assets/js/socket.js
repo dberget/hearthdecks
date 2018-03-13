2a6c3df68d2d9bae54e2459816cb7bb858e9c622
David Berget <davidberget@gmail.com>
2018-03-13 14:47:08 -0500

    cleaned up background

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -3,9 +3,4 @@ import { Socket } from "phoenix"
 let socket = new Socket("/socket")
 socket.connect()
 
-// let channel = socket.channel("room", {})
-// channel.join()
-//     .receive("ok", resp => { console.log("Joined successfully", resp) })
-//     .receive("error", resp => { console.log("Unable to join", resp) })
-
 export default socket
