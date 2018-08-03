87947e140d1d346bc70357ccac9ab87eff6b410d
David Berget <davidberget@gmail.com>
2018-08-03 15:01:45 -0500

    tweaks

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Hearthdecks.Mixfile do
     [
       app: :hearthdecks,
       version: "1.0.4",
-      elixir: "~> 1.6.1",
+      elixir: "~> 1.7.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
